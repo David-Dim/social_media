6dd33f43b59bf72906cb00e1ea4db6e56e3d5d01
gaspra <alanchik1@gmail.com>
2022-08-18 18:13:51 +0500

    layout

---
+++ src/components/Profile.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+const Profile = () => {
+  return (<div className="content">
+  <div>
+    <img
+      src="https://www.abercrombiekent.com/-/media/ak/media-for-prod/destinations/mastheads/americas-argentina-perito-moreno-glacier-mh.jpg?h=660&w=1920&la=en&hash=039B01BEB6D6035D041C4E68661B993E"
+      title="Global Hotness"
+      className="big-img"
+    ></img>
+  </div>
+  <div className="ava">
+    <img
+      src="https://images-platform.99static.com//dKP-vgUb3KMWmix2-FSCrRYHTY0=/204x210:748x754/fit-in/500x500/99designs-contests-attachments/137/137297/attachment_137297371"
+      className="profile-img1"
+      title="profile1"
+    ></img>
+    <h2>Users name</h2>
+    <p>ndjcksdcndksjcndjksnckjs</p>
+  </div>
+  <div className="posts">
+    <h2>My posts</h2>
+    <div>
+      <h3>Post 1</h3>
+    </div>
+    <div>
+      <h3>Post 2</h3>
+    </div>
+    <div>
+      <h3>Post 3</h3>
+    </div>
+    <div>
+      <h3></h3>
+    </div>
+  </div>
+</div>
+  )
+}
+export default Profile
